822c4f0bf59e5b1770523b847d7b7f6890d8c800
Roshan Sood <legodude2774@gmail.com>
2021-03-07 12:36:24 -0800

    Auto stash before revert of "Test code for learning in Java"

---
+++ something.java
@@ -0,0 +1,11 @@
+public class something{
+    public static void main(String[] args) {
+        Limelight limelight = new Limelight();
+        limelight.gettx();
+        System.out.println(limelight.gettx());
+
+        System.out.println("Hello World");
+        //Don't forget to have semi-colons!
+      
+    }
+}
